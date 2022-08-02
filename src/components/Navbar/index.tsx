0d790f6bac21980b995c37fe9ac199c5f7aae4c2
Juan Sebastian Artunduaga Rojas <jartunduaga7896@gmail.com>
2022-08-02 13:42:20 -0500

    Interfaz inicial [Maquetado]

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,12 @@
+export default function Navbar() {
+    return (
+        <div>
+            <div>
+                <h3>Title Program or image</h3>
+            </div>
+            <div>
+                <h3>Optional buttons</h3>
+            </div>
+        </div>
+    )
+}
