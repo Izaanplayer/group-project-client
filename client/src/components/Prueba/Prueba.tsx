2c8b5bef345f571cb757c0724572ca6b277f3e11
Maikyls <85658257+Maikyls@users.noreply.github.com>
2022-08-02 16:27:24 -0500

    Delete "pueba" folder

--- client/src/components/Prueba/Prueba.tsx
+++
@@ -1,8 +0,0 @@
-export default function Prueba () {
-    return (
-        <div className="container">
-            <div>Creo un nuevo div</div>
-            <h3>Cambio el holi por hola</h3>
-        </div>
-    )
-}
