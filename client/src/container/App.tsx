dac34ebe9dc3157f57798d2c8c5d7e5c70c9e366
Juan Sebastian Artunduaga Rojas <jartunduaga7896@gmail.com>
2022-08-04 13:51:08 -0500

    Se ordenan los componentes

--- client/src/container/App.tsx
+++ client/src/container/App.tsx
@@ -1,11 +1,11 @@
-import Example from '../components/Testing/Example';
+import Home from '../components/Home';
 import './App.css';
 
 function App() {
 
   return (
     <div>
-      <Example />
+      <Home />
     </div>
   );
 }
