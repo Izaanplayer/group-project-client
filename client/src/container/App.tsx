0e099d1b5f4ae4faeb84223e5a447ccad70acc07
Maikyls <85658257+Maikyls@users.noreply.github.com>
2022-08-04 14:27:27 -0500

    guardo cambios en archivo app

--- client/src/container/App.tsx
+++ client/src/container/App.tsx
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
 import Example from '../components/Testing/Example';
 import { Routes, Route, Link } from "react-router-dom";
-=======
 import Home from '../components/Home';
->>>>>>> dac34ebe9dc3157f57798d2c8c5d7e5c70c9e366
 import './App.css';
 import { ArtistDetail } from '../components/ArtistDetail/ArtistDetail';
 
@@ -11,14 +8,11 @@ function App() {
 
   return (
     <div>
-<<<<<<< HEAD
       <Routes>
         <Route path="/artistDetail" element={<ArtistDetail />} />
         <Route path='/' element={<Example />} />
       </Routes>
-=======
       <Home />
->>>>>>> dac34ebe9dc3157f57798d2c8c5d7e5c70c9e366
     </div>
   );
 }
