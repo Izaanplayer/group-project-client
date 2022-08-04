474144baf243a5410f77703e1d8af940339db519
Angiep89 <angie.p.perdomo@gmail.com>
2022-08-04 14:47:25 -0500

    Se crea componente de search bar

--- client/src/container/App.tsx
+++ client/src/container/App.tsx
@@ -1,11 +1,13 @@
 import Home from '../components/Home';
+import SearchBar from '../components/SearchBar/SearchBar';
 import './App.css';
 
 function App() {
 
   return (
     <div>
-      <Home />
+      {/* <Home /> */}
+      <SearchBar/>
     </div>
   );
 }
