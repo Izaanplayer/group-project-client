074144bd5e234ce62591f6278cb850f184c138ee
Esteban Velasquez <jestebanvelasquez22@gmail.com>
2022-08-05 12:18:33 -0500

    redux toolkit = ok

--- client/src/components/Prueba/Prueba.jsx
+++ client/src/components/Prueba/Prueba.jsx
@@ -1,9 +1,11 @@
 import React,{useEffect} from 'react';
 import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom';
 import { getarts } from '../../Redux/actions/artistAction';
 
 export default function Prueba() {
     const dispatch = useDispatch()
+    const history = useNavigate()
     const {artists} = useSelector(state => state.artists)
 
     useEffect(() => {
@@ -19,18 +21,25 @@ export default function Prueba() {
                     <div  className="max-w-sm rounded overflow-hidden shadow-lg container">
 
                         <h2>{ele.nickName}</h2>
-                        <p>{ele.name}</p>
+                        <h2>{ele.name}</h2>
                         <p>{ele.lastName}</p>
                         <p>{ele.city}</p>
                         <p>{ele.country}</p>
                         <p>{ele.description}</p>
                     </div>
 
+                    
 
 
                     </div>
                 )
             })}
+            <button onClick={() =>{history('/')}} class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+                        come back
+            </button>
         </div>
     )
 }
+
+
+//// @ts-ignore 
