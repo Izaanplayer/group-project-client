6d5e49bd727231744804fb092a21fd442924b951
Juan Sebastian Artunduaga Rojas <jartunduaga7896@gmail.com>
2022-08-06 02:56:28 -0500

    Se realiza el traslado de toda la funcionalidad & se añade react-redux + toolkit + typescript

---
+++ client/src/components/Prueba/index.tsx
@@ -0,0 +1,14 @@
+export default function Prueba() {
+    return (
+        <div className="h-56 grid grid-cols-3 gap-4 content-between ...">
+            <div>
+                <div className="max-w-sm rounded overflow-hidden shadow-lg container">
+                    <p>szs</p>
+                </div>
+            </div>
+            <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+                come back
+            </button>
+        </div>
+    )
+}
--- client/src/container/App.tsx
+++ client/src/container/App.tsx
@@ -1,22 +1,22 @@
-import { Routes, Route } from "react-router-dom";
-
-import Home from '../components/Home';
-import Example from '../components/Testing/Example';
-import ArtistDetail from '../components/ArtistDetail/ArtistDetail';
-
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import ArtistDetail from "../components/ArtistDetail/ArtistDetail";
+import Home from "../components/Home";
+import Prueba from "../components/Prueba";
+import Example from "../components/Testing/Example";
 
 function App() {
-
   return (
-    <div>
+    <>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path="/artistDetail" element={<ArtistDetail />} />
         <Route path='/testing' element={<Example />} />
+
+        <Route path='/prueba' element={<Prueba />} />
+
       </Routes>
-    </div>
-  );
+    </>
+  )
 }
 
 export default App;
