da99b02b2292cb5632619457155d94b06e5131c5
Maikyls <85658257+Maikyls@users.noreply.github.com>
2022-08-06 03:16:47 -0500

    se cambia nombre carpeta redux

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./Redux/store/store";
+import { store } from "./redux/store/store";
 
 import App from "./container/App";
 import './index.css';
