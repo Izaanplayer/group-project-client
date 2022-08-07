0a188d04e76d2e97edf310b99f6dce1ea7eef257
Juan Sebastian Artunduaga Rojas <jartunduaga7896@gmail.com>
2022-08-07 09:28:34 -0500

    Se guardan cambios para verificar información de otra rama

--- client/src/container/App.tsx
+++ client/src/container/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import ArtistDetail from "../components/ArtistDetail/ArtistDetail";
+import Explore from "../components/Explorar";
 import Home from "../components/Home";
 import Prueba from "../components/Prueba";
 import Example from "../components/Testing/Example";
@@ -9,6 +10,7 @@ export default function App() {
     <>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/explore' element={<Explore />} />
         <Route path='/artistDetail' element={<ArtistDetail />} />
         <Route path='/testing' element={<Example />} />
         <Route path='/prueba' element={<Prueba />} />
