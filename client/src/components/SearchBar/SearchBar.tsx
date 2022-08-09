52995277472af8afb06fd6ff4b22f41a065fb3e2
Angiep89 <angie.p.perdomo@gmail.com>
2022-08-08 19:47:28 -0500

    Componente search

--- client/src/components/SearchBar/SearchBar.tsx
+++ client/src/components/SearchBar/SearchBar.tsx
@@ -1,5 +1,21 @@
-export default function SearchBar() {
+//@ts-nocheck
+import { useState } from "react";
+import { useDispatch } from "react-redux"
+import React from "react";
 
+export default function SearchBar() {
+    const dispatch = useDispatch();
+    const [name, setName] = useState("");
+
+    function handleInputChange(e){
+        e.preventDefault();
+        setName(e.target.value)
+    }
+
+    function handleSubmit(e){
+        e.preventDefault()
+        dispatch()
+    }
     return (
         <>
             <div className="flex items-center">
@@ -8,11 +24,11 @@ export default function SearchBar() {
                     <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
                         <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
                     </div>
-                    <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-red-500 dark:focus:border-red-500" placeholder="Search ......." />
+                    <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-red-500 dark:focus:border-red-500" placeholder="Search ......." onChange={(e) => handleInputChange(e)}/>
                     <button type="button" className="flex absolute inset-y-0 right-0 items-center pr-3">
                     </button>
                 </div>
-                <button type="button" className="flex items-center py-2.5 px-3 ml-2 text-sm font-medium text-white bg-red-500 rounded-lg border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300">
+                <button type="button" className="flex items-center py-2.5 px-3 ml-2 text-sm font-medium text-white bg-red-500 rounded-lg border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300" onClick={(e) => handleSubmit(e)}>
                     <svg aria-hidden="true" className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                 </button>
             </div>
