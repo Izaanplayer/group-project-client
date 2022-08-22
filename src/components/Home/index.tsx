07b325972c7a6976aebc7cc619ba21d748b82221
Juan Sebastian Artunduaga Rojas <jartunduaga7896@gmail.com>
2022-08-21 19:43:25 -0500

    Se verifica el funcionamiento del deployment automático

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
                     </div>
                     <div className="w-1/2 px-7">
                         <h1 className="text-end text-gray-500 font-extrabold text-6xl mb-6">Artist App</h1>
-                        <p className="my-4 text-lg text-gray-700">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam incidunt, quia et qui aut quo eaque molestias itaque placeat! Nihil molestiae eligendi nemo tenetur quia ratione aut maiores aspernatur recusandae.</p>
+                        <p className="my-4 text-lg text-gray-700">Se realiza este cambio para validar el funcionamiento del despliegue automático desde Firebase.</p>
                         <Link to="/artists">
                             <button className="inline-block px-10 py-2 ml-2 text-sm font-medium text-white bg-red-500 rounded-lg border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 float-right">Descubrir artistas 🙌</button>
                         </Link>
